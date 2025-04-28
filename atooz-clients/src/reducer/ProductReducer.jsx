afc28cc006c6838cdda752de6e940e6f88b43a96
rony <ha2ronyahamed@gmail.com>
2025-04-28 23:35:21 +0600

    atooz-ecommerce-pract

---
+++ atooz-clients/src/reducer/ProductReducer.jsx
@@ -0,0 +1,68 @@
+// import React from 'react'
+
+// const ProductReducer = () => {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default ProductReducer
+
+const ProductReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_LOADING":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+
+    // Address module
+    case "SET_API_DISTRICT":
+      return {
+        ...state,
+        isLoading: false,
+        district: action.payload,
+        isError: false,
+      };
+
+    case "SET_API_UNPAGINATE_UPAZILA":
+      return {
+        ...state,
+        isLoading: false,
+        upazila: action.payload,
+        isError: false,
+      };
+
+    // Product module
+    case "SET_API_CATEGORY":
+      return {
+        ...state,
+        isLoading: false,
+        category: action.payload,
+        isError: false,
+      };
+
+    case "SET_API_UNPAGINATE_CATEGORY":
+      return {
+        ...state,
+        isLoading: false,
+        unpaginate_category: action.payload,
+        isError: false,
+      };
+
+    case "API_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default ProductReducer;
