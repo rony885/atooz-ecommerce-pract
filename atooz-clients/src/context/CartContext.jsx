98be6869699c245a41442fa0517d2b44f67524f4
rony <ha2ronyahamed@gmail.com>
2025-05-11 23:55:09 +0600

    atooz-ecommerce-pract

--- atooz-clients/src/context/CartContext.jsx
+++ atooz-clients/src/context/CartContext.jsx
@@ -3,16 +3,40 @@ import reducer from "../reducer/CartReducer";
 
 const CartContext = createContext();
 
-const initialState = {
-  //   wishlist: getLocalWislistData(),
+const getLocalWislistData = () => {
+  let localWishlistData = localStorage.getItem("atoozWishlist");
 
-  //   cart: getLocalCartData(),
+  const parsedData = JSON.parse(localWishlistData);
+  if (!Array.isArray(parsedData)) return [];
+  return parsedData;
+};
+
+const getLocalCartData = () => {
+  let localCartData = localStorage.getItem("atoozCart");
+
+  const parsedData = JSON.parse(localCartData);
+  if (!Array.isArray(parsedData)) return [];
+  return parsedData;
+};
+
+const getLocalOrderData = () => {
+  let localOrderData = localStorage.getItem("atoozOrder");
+
+  const parsedData = JSON.parse(localOrderData);
+  if (!parsedData || typeof parsedData !== "object") return {};
+  return parsedData;
+};
+
+const initialState = {
+  wishlist: getLocalWislistData(),
+
+  cart: getLocalCartData(),
   total_item: "",
   total_price: "",
   total_discount: "",
   total_special_price: "",
 
-  //   order: getLocalOrderData(),
+  order: getLocalOrderData(),
   order_total_price: "",
   order_total_discount: "",
 
--- atooz-clients/src/index.js
+++ atooz-clients/src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AppProvider } from "./context/ProductContext";
+import { CartProvider } from "./context/CartContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </AppProvider>
   </React.StrictMode>
 );
 
