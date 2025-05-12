b9f16489c37a6fe12308bfe1533757778df72819
rony <ha2ronyahamed@gmail.com>
2025-05-12 23:45:55 +0600

    atooz-ecommerce-pract

--- atooz-clients/src/context/CartContext.jsx
+++ atooz-clients/src/context/CartContext.jsx
@@ -45,6 +45,51 @@ const initialState = {
 const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // add to Wishlist
+  const addToWishlist = (id, product) => {
+    dispatch({
+      type: "ADD_TO_WISHLIST",
+      payload: { id, product },
+    });
+  };
+
+  // remove to Wishlist
+  const removeWishlistItem = (id) => {
+    dispatch({ type: "REMOVE_WISHLIST_ITEM", payload: id });
+  };
+
+  // to clear the Wishlist
+  const clearWishlist = () => {
+    dispatch({ type: "CLEAR_WISHLIST" });
+  };
+
+  // add to cart
+  const addToCart = (id, amount, size, color, product) => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: { id, amount, size, color, product },
+    });
+  };
+
+  // remove to cart
+  const removeItem = (id) => {
+    dispatch({ type: "REMOVE_ITEM", payload: id });
+  };
+
+  // to clear the cart
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  // increase & decrease
+  const setDecrement = (id) => {
+    dispatch({ type: "SET_DECREMENT", payload: id });
+  };
+
+  const setIncrement = (id) => {
+    dispatch({ type: "SET_INCREMENT", payload: id });
+  };
+
   return (
     <CartContext.Provider
       value={{
