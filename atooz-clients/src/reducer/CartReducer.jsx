d37eac66aefc840bda95a968eee5d977106ff021
rony <ha2ronyahamed@gmail.com>
2025-05-15 23:58:02 +0600

    atooz-ecommerce-pract

--- atooz-clients/src/reducer/CartReducer.jsx
+++ atooz-clients/src/reducer/CartReducer.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
 
-const CartReducer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const CartReducer = ({ state, action }) => {
+  //   wishlist
+  if (action.type === "ADD_TO_WISHLIST") {
+    let { id, product } = action.payload;
+
+    // Check if the product is already in the wishlist
+    let existingProduct = state.wishlist.find((curItem) => curItem.id === id);
+
+    if (existingProduct) {
+      // If the product already exists, return the current state (no duplicates)
+      return {
+        ...state,
+        wishlist: state.wishlist, // No changes
+      };
+    } else {
+      // If the product doesn't exist in the wishlist, add it
+      let wishlistProduct = {
+        id: id,
+        name: product.name,
+        image: product.mainImage,
+        price: Number(product.specialPrice),
+        discount: Number(product.discount),
+      };
+
+      return {
+        ...state,
+        wishlist: [...state.wishlist, wishlistProduct], // Add new item to the wishlist
+      };
+    }
+  }
+
+  if (action.type === "REMOVE_WISHLIST_ITEM") {
+    let updatedWishlist = state.wishlist.filter(
+      (curElem) => curElem.id !== action.payload
+    );
+
+    return {
+      ...state,
+      wishlist: updatedWishlist,
+    };
+  }
+
+  if (action.type === "CLEAR_WISHLIST") {
+    return {
+      ...state,
+      wishlist: [],
+    };
+  }
+
+  return state;
 }
 
 export default CartReducer
